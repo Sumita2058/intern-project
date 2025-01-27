ee087c4aba3dbd1dc4fa05aa2a1f0d993407fd6e
= <=>
2025-01-26 19:55:11 -0800

    add the project

---
+++ sidebar/script.js
@@ -0,0 +1,14 @@
+// const navdialogue = document.getElementById("#nav-dialogue")
+// function handlemenu()
+
+// {
+//     navdialogue.classList.toggle("hidden");
+//     navdialogue.classList.toggle("show");
+// }
+
+
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector("#sidebar");
+function handlemenu(){
+    sidebar.classList.toggle('active');
+}
